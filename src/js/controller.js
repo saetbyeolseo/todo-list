a717c90a5fc1f413c9ce302f3ec3c55b37efb318
saetbyeolseo <ettett25@gmail.com>
2023-05-13 19:42:36 +0900

    feature: 2초간 머무를 경우 미리보기를 제공합니다.

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,14 +1,16 @@
 import utils from './utils';
-import store from './store';
+import storage from './storage';
 import template from './template';
 
 const $todo = utils.$qs('.todo');
 const $todoList = utils.$qs('.todo-list');
 const $footer = utils.$qs('.footer');
+let dragTargetId = null;
+let timer = null;
 
-const update = () => {
+const update = (preview = []) => {
   const filter = document.location.hash.replace(/^#/, '');
-  const totalList = store.getItem();
+  const totalList = preview.length ? preview : storage.getItem();
   const completedList = totalList.filter((item) => item.completed);
   const activeList = totalList.filter((item) => !item.completed);
   const target = filter === '' ? totalList : filter === 'active' ? activeList : completedList;
@@ -16,14 +18,36 @@ const update = () => {
   $footer.innerHTML = template.filters(activeList.length, filter, completedList.length);
 };
 
+const sortList = (e, preview = false) => {
+  const dragCurrentId = parseInt(e.target.dataset.id || e.target.parentNode.dataset.id);
+  if (dragTargetId && dragCurrentId && dragTargetId !== dragCurrentId) {
+    const totalList = storage.getItem();
+    const targetItem = totalList.find((item) => item.id === dragTargetId);
+    const currentItemSort = totalList.findIndex((item) => item.id === dragCurrentId);
+    if (targetItem) {
+      const filterList = totalList.filter((item) => item.id !== dragTargetId);
+      const newList = [...filterList.slice(0, currentItemSort), targetItem, ...filterList.slice(currentItemSort)];
+      const sortLst = newList.map((e, i) => {
+        e.sort = newList.length - (1 + i);
+        return e;
+      });
+      if (!preview) {
+        storage.setItem(sortLst);
+      } else {
+        update(sortLst);
+      }
+    }
+  }
+};
+
 const bindEvent = () => {
   const { $on, $delegate } = utils;
   $on($todo, 'change', ({ target }) => {
-    store.setItem([
-      ...store.getItem(),
+    storage.setItem([
+      ...storage.getItem(),
       {
         id: Date.now(),
-        sort: store.getItem().length,
+        sort: storage.getItem().length,
         title: target.value,
         completed: false,
       },
@@ -32,29 +56,9 @@ const bindEvent = () => {
     target.value = '';
   });
 
-  $delegate($todoList, '.delete', 'click', ({ target }) => {
-    const id = parseInt(target.parentNode.dataset.id);
-    const itemList = store.getItem();
-    store.setItem(itemList.filter((item) => item.id !== id));
-    update();
-  });
-
-  $delegate($todoList, '.todo-item', 'click', ({ target }) => {
-    const id = parseInt(target.parentNode.dataset.id);
-    let itemList = store.getItem();
-    itemList = itemList.map((item) => {
-      if (item.id === id) {
-        item.completed = !item.completed;
-      }
-      return item;
-    });
-    store.setItem(itemList);
-    update();
-  });
-
   $delegate($footer, '.clear-completed', 'click', () => {
-    const totalList = store.getItem();
-    store.setItem(totalList.filter((item) => !item.completed));
+    const totalList = storage.getItem();
+    storage.setItem(totalList.filter((item) => !item.completed));
     update();
   });
 
@@ -62,47 +66,63 @@ const bindEvent = () => {
     setTimeout(() => update(), 100);
   });
 
-  let dragTargetId = null;
+  $delegate($todoList, '.delete', 'click', ({ target }) => {
+    const id = parseInt(target.parentNode.dataset.id);
+    const itemList = storage.getItem();
+    storage.setItem(itemList.filter((item) => item.id !== id));
+    update();
+  });
+
+  $delegate($todoList, '.todo-item', 'click', ({ target }) => {
+    const id = parseInt(target.parentNode.dataset.id);
+    let itemList = storage.getItem();
+    itemList = itemList.map((item) => {
+      if (item.id === id) {
+        item.completed = !item.completed;
+      }
+      return item;
+    });
+    storage.setItem(itemList);
+    update();
+  });
 
   $on($todoList, 'dragstart', (e) => {
     dragTargetId = parseInt(e.target.dataset.id || e.target.parentNode.dataset.id);
     e.target.classList.add('dragItem');
   });
+
   $on($todoList, 'dragover', (e) => {
     e.preventDefault();
   });
+
   $on($todoList, 'dragenter', (e) => {
     if (e.target.classList.contains('todo-list__item')) {
       e.target.classList.add('dragOver');
+      timer = setTimeout(() => sortList(e, true), 2000);
     }
   });
+
   $on($todoList, 'dragleave', (e) => {
     if (e.target.classList.contains('todo-list__item')) {
       e.target.classList.remove('dragOver');
+      clearTimeout(timer);
     }
   });
+
   $on($todoList, 'dragend', (e) => {
     e.preventDefault();
     e.target.classList.remove('dragItem');
     dragTargetId = null;
   });
+
   $on($todoList, 'drop', (e) => {
-    const dragCurrentId = parseInt(e.target.dataset.id || e.target.parentNode.dataset.id);
-    if (dragTargetId && dragCurrentId && dragTargetId !== dragCurrentId) {
-      const totalList = store.getItem();
-      const targetItem = totalList.find((item) => item.id === dragTargetId);
-      const currentItemSort = totalList.findIndex((item) => item.id === dragCurrentId);
-      if (targetItem) {
-        const filterList = totalList.filter((item) => item.id !== dragTargetId);
-        const newList = [...filterList.slice(0, currentItemSort), targetItem, ...filterList.slice(currentItemSort)];
-        store.setItem(
-          [...newList].map((e, i) => {
-            e.sort = newList.length - (1 + i);
-            return e;
-          })
-        );
-      }
-      update();
+    sortList(e);
+    update();
+  });
+
+  $on($todoList, 'keydown', (e) => {
+    if (e.key === 'Escape') {
+      document.dispatchEvent(new Event('mouseup'));
     }
   });
 };
